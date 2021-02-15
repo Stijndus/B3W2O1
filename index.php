9f68c6e34dd138e0285411c8154ca8cd95d9d9a9
Stijn <stijn@ardusit.nl>
2021-02-16 00:56:29 +0100

    Commit #2

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link>
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="content">
         <h1>Mad Libs</h1>
-        <form>
-
-        </form>
+        <section class="wrapper">
+            <form>
+                <label for="quest1">Wat zou je graag willen kunnen?</label>
+                <input type="text" id="quest1" name="quest1">
+                <label for="quest2">Met welke persoon kun je goed opschieten</label>
+                <input type="text" id="quest2" name="quest2">
+                <label for="quest3">Wat is je favoriete getal?</label>
+                <input type="text" id="quest3" name="quest3">
+                <label for="quest4">Wat heb je altijd bij je als je op vakantie gaat?</label>
+                <input type="text" id="quest4" name="quest4">
+                <label for="quest5">Wat is je beste persoonlijke eigenschap?</label>
+                <input type="text" id="quest5" name="quest5">
+                <label for="quest6">Wat is je slechtste persoonlijke eigenschap</label>
+                <input type="text" id="quest6" name="quest6">
+                <label for="quest7">Wat is het ergste dat je kan overkomen?</label>
+                <input type="text" id="quest6" name="quest7">
+            </form>
+        </section>
     </div>
 </body>
 </html>
